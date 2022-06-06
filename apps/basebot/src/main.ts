bfacc623323d7f86f0aa41ed7dc25e7b44ba1f09
Coby Benveniste <coby@base.la>
2022-06-06 11:42:40 +0300

    Add in metrics to test

--- apps/basebot/src/main.ts
+++ apps/basebot/src/main.ts
@@ -3,16 +3,25 @@ import { loadEnvs } from '@base/env';
 import { environment } from './environments/environment';
 loadEnvs(environment, ['configs', 'secrets']);
 
+import {
+  PrometheusReporter,
+  expressMetricsEndpoint,
+  expressHttpMetricsMiddleware,
+} from '@base/metrics';
 import * as express from 'express';
 import { logger } from '@base/logger';
 
 const app = express();
+const metricsReporter = new PrometheusReporter();
+
+app.use(expressHttpMetricsMiddleware(metricsReporter));
+app.get('/metrics', expressMetricsEndpoint(metricsReporter));
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to basebot!' });
 });
 
-const port = process.env.port || 3333;
+const port = process.env['PORT'] || 3333;
 const server = app.listen(port, () => {
   logger.info(`Listening at http://localhost:${port}/api`);
 });
