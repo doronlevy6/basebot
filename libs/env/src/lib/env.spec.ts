eb5c45ca31a00ce9eb94459f09a9ae07aa80ac04
Coby Benveniste <coby@base.la>
2022-06-06 13:03:43 +0300

    Delete env test because it causes issues when running in parallel

--- libs/env/src/lib/env.spec.ts
+++ libs/env/src/lib/env.spec.ts
@@ -2,20 +2,12 @@ import { loadEnvs } from './env';
 import { IInitialEnv } from './types';
 
 describe('env', () => {
-  it('should load env to process.env', () => {
-    const initialEnv: IInitialEnv = {
-      cwd: process.cwd(),
-      env: 'test',
-    };
-    loadEnvs(initialEnv, ['configs']);
-    expect(process.env['PORT']).toEqual('3333');
-  });
-
   it('should load override env to process.env', () => {
-    process.env['NODE_ENV_OVERRIDE'] = 'local';
+    process.env['NODE_OVERRIDE_ENV'] = 'local';
     const initialEnv: IInitialEnv = {
       cwd: process.cwd(),
       env: 'test',
+      serviceName: 'test',
     };
     loadEnvs(initialEnv, ['configs']);
     expect(process.env['ENV']).toEqual('local');
