be1f505c21d0f3118f39a45b9d50424c4950160d
Coby Benveniste <coby@base.la>
2022-06-08 13:20:01 +0300

    First pass at formatting slack

--- apps/slackbot/src/task-status/manager.ts
+++ apps/slackbot/src/task-status/manager.ts
@@ -1,13 +1,18 @@
 import { logger } from '@base/logger';
 import { Job, Worker } from 'bullmq';
 import { createQueueWorker, IQueueConfig } from '@base/queues';
-import { WebClient } from '@slack/web-api';
+import {
+  PlainTextOption,
+  SectionBlock,
+  UsersLookupByEmailResponse,
+  WebClient,
+} from '@slack/web-api';
 import { PgInstallationStore } from '../installations/installationStore';
+import { Task, User } from '@base/oapigen';
 
 interface TaskStatusJob {
-  organizationId: string;
-  userId: string;
-  taskId: string;
+  user: User;
+  task: Task;
 }
 
 export class TaskStatusManager {
@@ -51,14 +56,92 @@ export class TaskStatusManager {
 
   private async requestTaskStatus(job: Job<TaskStatusJob>) {
     const installation = await this.installationStore.fetchInstallationByBaseId(
-      job.data.organizationId,
+      job.data.user.organizationId,
     );
 
     const client = new WebClient(installation.bot.token);
+    const assigneeSlackUserRes = await client.users.lookupByEmail({
+      email: job.data.user.email,
+    });
+    if (assigneeSlackUserRes.error) {
+      throw new Error(
+        `Error getting assigned user by email in slack: ${assigneeSlackUserRes.error}`,
+      );
+    }
+
+    if (!assigneeSlackUserRes.ok) {
+      throw new Error(`Error getting assigned user by email in slack`);
+    }
+
+    const taskCreatorSlackUserRes = await client.users.lookupByEmail({
+      email: job.data.task.creator.email,
+    });
+    if (taskCreatorSlackUserRes.error) {
+      throw new Error(
+        `Error getting task creator user by email in slack: ${taskCreatorSlackUserRes.error}`,
+      );
+    }
+
+    if (!taskCreatorSlackUserRes.ok) {
+      throw new Error(`Error getting task creator user by email in slack`);
+    }
+
     client.chat.postMessage({
-      channel: '', // TODO: Get user ID
-      text: `An update is requested for task id ${job.data.taskId}`, // TODO: Design
+      channel: assigneeSlackUserRes.user.id,
+      text: `Hi ${job.data.user.displayName}, you have a task status update request!`,
+      blocks: this.getFormattedBlocks(
+        assigneeSlackUserRes,
+        taskCreatorSlackUserRes,
+        job.data.user.id,
+        job.data.user.organizationId,
+        job.data.task.id,
+        ['Reassigned to someone else', 'Not started', 'In Progress', 'Done'],
+      ),
     });
     logger.info({ msg: 'receive task status request', job: job.data });
+  }
+
+  private getFormattedBlocks(
+    assignedUserRes: UsersLookupByEmailResponse,
+    taskCreatorUserRes: UsersLookupByEmailResponse,
+    baseUser: string,
+    baseOrg: string,
+    taskId: string,
+    value: string[],
+  ): SectionBlock[] {
+    const valuesToSendArr = [baseOrg, baseUser, taskId];
+
+    const options = value.map((val) => {
+      const a: PlainTextOption = {
+        text: {
+          type: 'plain_text',
+          text: val,
+        },
+        value: JSON.stringify(valuesToSendArr),
+      };
+      return a;
+    });
+
+    return [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Hi <${assignedUserRes.user.id}>, you have a task status update request! <${taskCreatorUserRes.user.id}> created this task for you which is due in X days/weeks.`,
+        },
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: 'What is the current status of this task?',
+        },
+        accessory: {
+          type: 'static_select',
+          options: options,
+          action_id: 'static_select-action',
+        },
+      },
+    ];
   }
 }
