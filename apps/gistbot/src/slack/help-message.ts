cce3d562debbf756a10e8edd4126a08fda3074b7
Itay Dressler <itay@base.la>
2022-09-29 12:12:33 +0300

    update help message (#82)

--- apps/gistbot/src/slack/help-message.ts
+++ apps/gistbot/src/slack/help-message.ts
@@ -1,5 +1,7 @@
 import { RespondFn } from '@slack/bolt';
 
+const IMAGE_URL = 'https://assets.base.la/gist/assets/welcomeMessage.jpg';
+
 export const respondWithHelp = async (respond: RespondFn) => {
   await respond({
     response_type: 'ephemeral',
@@ -8,25 +10,31 @@ export const respondWithHelp = async (respond: RespondFn) => {
         type: 'section',
         text: {
           type: 'mrkdwn',
-          text: `Hi there :wave: here are some ideas of what you can do:`,
+          text:
+            'Hi there :wave:' +
+            '\n' +
+            'here are some ideas of what you can do:' +
+            '\n' +
+            '*Summarize channels:*' +
+            '\n\n' +
+            `Use \`/gist\` from any channel to get a fresh summary of what happened in that channel.` +
+            '\n' +
+            `Use \`/gist #channel1 #channel2\` to get a summary of what happened in those channels.` +
+            '\n\n' +
+            '*Summarize threads:*' +
+            '\n\n' +
+            'Use the message shortcut to summarize a message or a thread (see image below)',
         },
       },
       {
-        type: 'divider',
-      },
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: 'Use `/gist` from any channel to get a fresh summary of what happened in that channel.',
+        type: 'image',
+        title: {
+          type: 'plain_text',
+          text: 'image1',
+          emoji: true,
         },
-      },
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: 'Use `/gist #channel1 #channel2` to get a summary of what happened in those channels.',
-        },
+        image_url: IMAGE_URL,
+        alt_text: 'image1',
       },
     ],
   });
