c9a85e128cf14d83abff3ab39987fddd69ed1033
Coby Benveniste <41164052+probably-not@users.noreply.github.com>
2022-11-17 11:09:29 +0200

    Don't pop modal when channel is a public channel (#239)
    * Don't pop modal when channel is a public channel
    
    * Add re-run

--- apps/gistbot/src/summaries/channel-handler.ts
+++ apps/gistbot/src/summaries/channel-handler.ts
@@ -1,10 +1,130 @@
-import { addToChannelInstructions } from '../slack/add-to-channel';
+import { addToChannel } from '../slack/add-to-channel';
 import { SlackSlashCommandWrapper } from '../slack/types';
 import { AnalyticsManager } from '../analytics/manager';
 import { privateChannelInstructions } from '../slack/private-channel';
 import { identifyTriggeringUser } from '../slack/utils';
 import { ChannelSummarizer } from './channel/channel-summarizer';
 import { extractDaysBack, summaryInProgressMessage } from './utils';
+import { RespondFn, SlashCommand, Context } from '@slack/bolt';
+import { Logger, WebClient } from '@slack/web-api';
+
+const runSlashCommand = async (
+  analyticsManager: AnalyticsManager,
+  channelSummarizer: ChannelSummarizer,
+  client: WebClient,
+  logger: Logger,
+  payload: SlashCommand,
+  respond: RespondFn,
+  context: Context,
+) => {
+  try {
+    analyticsManager.channelSummaryFunnel({
+      funnelStep: 'user_requested',
+      slackTeamId: payload.team_id,
+      slackUserId: payload.user_id,
+      channelId: payload.channel_id,
+    });
+
+    const { text, channel_id, user_id, channel_name, team_id } = payload;
+
+    // Don't await so that we don't force anything to wait just for the identification.
+    // This handles error handling internally and will never cause an exception, so we
+    // won't have any unhandled promise rejection errors.
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    identifyTriggeringUser(user_id, team_id, client, analyticsManager);
+
+    const daysBack = extractDaysBack(text);
+
+    await summaryInProgressMessage(client, {
+      channel: payload.channel_id,
+      user: payload.user_id,
+      daysBack: daysBack,
+    });
+
+    analyticsManager.channelSummaryFunnel({
+      funnelStep: 'in_progress_sent',
+      slackTeamId: payload.team_id,
+      slackUserId: payload.user_id,
+      channelId: payload.channel_id,
+    });
+    logger.info(
+      `${user_id} requested a channel summarization on ${channel_name} for ${daysBack} days`,
+    );
+
+    await channelSummarizer.summarize(
+      'slash_command',
+      context.botId || '',
+      team_id,
+      user_id,
+      {
+        type: 'channel',
+        channelId: channel_id,
+        channelName: channel_name,
+      },
+      daysBack,
+      client,
+      respond,
+    );
+  } catch (error) {
+    logger.error(`error in channel summarization: ${error} ${error.stack}`);
+
+    if ((error as Error).message.toLowerCase().includes('not_in_channel')) {
+      await addToChannel(
+        client,
+        {
+          teamId: payload.team_id,
+          channelId: payload.channel_id,
+          currentUser: payload.user_id,
+        },
+        analyticsManager,
+      );
+
+      analyticsManager.channelSummaryFunnel({
+        funnelStep: 'added_self_to_channel',
+        slackTeamId: payload.team_id,
+        slackUserId: payload.user_id,
+        channelId: payload.channel_id,
+      });
+
+      await runSlashCommand(
+        analyticsManager,
+        channelSummarizer,
+        client,
+        logger,
+        payload,
+        respond,
+        context,
+      );
+
+      return;
+    }
+
+    if (
+      (error as Error).message.toLowerCase().includes('channel_not_found') ||
+      (error as Error).message.toLowerCase().includes('missing_scope')
+    ) {
+      await privateChannelInstructions(
+        client,
+        payload.trigger_id,
+        {
+          channelId: payload.channel_id,
+          channelName: payload.channel_name,
+          currentUser: payload.user_id,
+          teamId: payload.team_id,
+        },
+        analyticsManager,
+        context.botUserId || '',
+      );
+      analyticsManager.channelSummaryFunnel({
+        funnelStep: 'private_channel',
+        slackTeamId: payload.team_id,
+        slackUserId: payload.user_id,
+        channelId: payload.channel_id,
+      });
+      return;
+    }
+  }
+};
 
 export const channelSummarizationHandler =
   (analyticsManager: AnalyticsManager, channelSummarizer: ChannelSummarizer) =>
@@ -18,107 +138,18 @@ export const channelSummarizationHandler =
   }: SlackSlashCommandWrapper) => {
     try {
       await ack();
-
-      analyticsManager.channelSummaryFunnel({
-        funnelStep: 'user_requested',
-        slackTeamId: payload.team_id,
-        slackUserId: payload.user_id,
-        channelId: payload.channel_id,
-      });
-
-      const { text, channel_id, user_id, channel_name, team_id } = payload;
-
-      // Don't await so that we don't force anything to wait just for the identification.
-      // This handles error handling internally and will never cause an exception, so we
-      // won't have any unhandled promise rejection errors.
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      identifyTriggeringUser(user_id, team_id, client, analyticsManager);
-
-      const daysBack = extractDaysBack(text);
-
-      await summaryInProgressMessage(client, {
-        channel: payload.channel_id,
-        user: payload.user_id,
-        daysBack: daysBack,
-      });
-
-      analyticsManager.channelSummaryFunnel({
-        funnelStep: 'in_progress_sent',
-        slackTeamId: payload.team_id,
-        slackUserId: payload.user_id,
-        channelId: payload.channel_id,
-      });
-      logger.info(
-        `${user_id} requested a channel summarization on ${channel_name} for ${daysBack} days`,
-      );
-
-      await channelSummarizer.summarize(
-        'slash_command',
-        context.botId || '',
-        team_id,
-        user_id,
-        {
-          type: 'channel',
-          channelId: channel_id,
-          channelName: channel_name,
-        },
-        daysBack,
+      await runSlashCommand(
+        analyticsManager,
+        channelSummarizer,
         client,
+        logger,
+        payload,
         respond,
+        context,
       );
     } catch (error) {
-      logger.error(`error in channel summarization: ${error} ${error.stack}`);
-
-      if ((error as Error).message.toLowerCase().includes('not_in_channel')) {
-        await addToChannelInstructions(
-          client,
-          payload.trigger_id,
-          {
-            channelId: payload.channel_id,
-            channelName: payload.channel_name,
-            currentUser: payload.user_id,
-            teamId: payload.team_id,
-            summarization: {
-              type: 'channel',
-              channelId: payload.channel_id,
-              channelName: payload.channel_name,
-            },
-          },
-          analyticsManager,
-          context.botUserId || '',
-        );
-        analyticsManager.channelSummaryFunnel({
-          funnelStep: 'not_in_channel',
-          slackTeamId: payload.team_id,
-          slackUserId: payload.user_id,
-          channelId: payload.channel_id,
-        });
-        return;
-      }
-
-      if (
-        (error as Error).message.toLowerCase().includes('channel_not_found') ||
-        (error as Error).message.toLowerCase().includes('missing_scope')
-      ) {
-        await privateChannelInstructions(
-          client,
-          payload.trigger_id,
-          {
-            channelId: payload.channel_id,
-            channelName: payload.channel_name,
-            currentUser: payload.user_id,
-            teamId: payload.team_id,
-          },
-          analyticsManager,
-          context.botUserId || '',
-        );
-        analyticsManager.channelSummaryFunnel({
-          funnelStep: 'private_channel',
-          slackTeamId: payload.team_id,
-          slackUserId: payload.user_id,
-          channelId: payload.channel_id,
-        });
-        return;
-      }
+      logger.error(
+        `error in channel summarization handler: ${error} ${error.stack}`,
+      );
     }
   };
--- apps/gistbot/src/summaries/thread-handler.ts
+++ apps/gistbot/src/summaries/thread-handler.ts
@@ -1,10 +1,141 @@
 import { SlackShortcutWrapper } from '../slack/types';
-import { addToChannelInstructions } from '../slack/add-to-channel';
+import { addToChannel } from '../slack/add-to-channel';
 import { summaryInProgressMessage } from './utils';
 import { AnalyticsManager } from '../analytics/manager';
 import { privateChannelInstructions } from '../slack/private-channel';
 import { identifyTriggeringUser } from '../slack/utils';
 import { ThreadSummarizer } from './thread/thread-summarizer';
+import { Logger, WebClient } from '@slack/web-api';
+import { Context, MessageShortcut, RespondFn } from '@slack/bolt';
+
+export const runShortcut = async (
+  analyticsManager: AnalyticsManager,
+  threadSummarizer: ThreadSummarizer,
+  client: WebClient,
+  logger: Logger,
+  shortcut: MessageShortcut,
+  payload: MessageShortcut,
+  respond: RespondFn,
+  context: Context,
+) => {
+  try {
+    if (!payload.message.user && !payload.message['bot_id']) {
+      throw new Error('cannot extract user from empty user');
+    }
+
+    analyticsManager.threadSummaryFunnel({
+      funnelStep: 'user_requested',
+      slackTeamId: payload.team?.id || 'unknown',
+      slackUserId: payload.user.id,
+      channelId: payload.channel.id,
+      threadTs: payload.message_ts,
+    });
+
+    await summaryInProgressMessage(client, {
+      channel: payload.channel.id,
+      user: payload.user.id,
+      thread_ts: payload.message_ts,
+    });
+
+    analyticsManager.threadSummaryFunnel({
+      funnelStep: 'in_progress_sent',
+      slackTeamId: payload.team?.id || 'unknown',
+      slackUserId: payload.user.id,
+      channelId: payload.channel.id,
+      threadTs: payload.message_ts,
+    });
+
+    // Don't await so that we don't force anything to wait just for the identification.
+    // This handles error handling internally and will never cause an exception, so we
+    // won't have any unhandled promise rejection errors.
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    identifyTriggeringUser(
+      payload.user.id,
+      payload.team?.id || 'unknown',
+      client,
+      analyticsManager,
+    );
+
+    logger.info(
+      `${shortcut.user.id} requested a thread summarization on ${payload.message.ts} in channel ${payload.channel.id}`,
+    );
+
+    await threadSummarizer.summarize(
+      context.botId || '',
+      payload.team?.id || 'unknown',
+      payload.user.id,
+      {
+        type: 'thread',
+        threadTs: payload.message_ts,
+        channelId: payload.channel.id,
+        channelName: payload.channel.name,
+      },
+      client,
+      respond,
+    );
+  } catch (error) {
+    logger.error(`error in thread summarization: ${error.stack}`);
+
+    if ((error as Error).message.toLowerCase().includes('not_in_channel')) {
+      await addToChannel(
+        client,
+        {
+          teamId: payload.team?.id || 'unknown',
+          channelId: payload.channel.id,
+          currentUser: payload.user.id,
+        },
+        analyticsManager,
+      );
+
+      analyticsManager.threadSummaryFunnel({
+        funnelStep: 'added_self_to_channel',
+        slackTeamId: payload.team?.id || 'unknown',
+        slackUserId: payload.user.id,
+        channelId: payload.channel.id,
+        threadTs: payload.message_ts,
+      });
+
+      await runShortcut(
+        analyticsManager,
+        threadSummarizer,
+        client,
+        logger,
+        shortcut,
+        payload,
+        respond,
+        context,
+      );
+
+      return;
+    }
+
+    if (
+      (error as Error).message.toLowerCase().includes('channel_not_found') ||
+      (error as Error).message.toLowerCase().includes('missing_scope')
+    ) {
+      await privateChannelInstructions(
+        client,
+        payload.trigger_id,
+        {
+          channelId: payload.channel.id,
+          channelName: payload.channel.name,
+          currentUser: payload.user.id,
+          teamId: payload.user.team_id || 'unknown',
+        },
+        analyticsManager,
+        context.botUserId || '',
+      );
+      analyticsManager.threadSummaryFunnel({
+        funnelStep: 'private_channel',
+        slackTeamId: payload.team?.id || 'unknown',
+        slackUserId: payload.user.id,
+        channelId: payload.channel.id,
+        threadTs: payload.message_ts,
+      });
+      return;
+    }
+  }
+};
 
 export const threadSummarizationHandler =
   (analyticsManager: AnalyticsManager, threadSummarizer: ThreadSummarizer) =>
@@ -19,117 +150,17 @@ export const threadSummarizationHandler =
   }: SlackShortcutWrapper) => {
     try {
       await ack();
-
-      if (!payload.message.user && !payload.message['bot_id']) {
-        throw new Error('cannot extract user from empty user');
-      }
-
-      analyticsManager.threadSummaryFunnel({
-        funnelStep: 'user_requested',
-        slackTeamId: payload.team?.id || 'unknown',
-        slackUserId: payload.user.id,
-        channelId: payload.channel.id,
-        threadTs: payload.message_ts,
-      });
-
-      await summaryInProgressMessage(client, {
-        channel: payload.channel.id,
-        user: payload.user.id,
-        thread_ts: payload.message_ts,
-      });
-
-      analyticsManager.threadSummaryFunnel({
-        funnelStep: 'in_progress_sent',
-        slackTeamId: payload.team?.id || 'unknown',
-        slackUserId: payload.user.id,
-        channelId: payload.channel.id,
-        threadTs: payload.message_ts,
-      });
-
-      // Don't await so that we don't force anything to wait just for the identification.
-      // This handles error handling internally and will never cause an exception, so we
-      // won't have any unhandled promise rejection errors.
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      identifyTriggeringUser(
-        payload.user.id,
-        payload.team?.id || 'unknown',
-        client,
+      await runShortcut(
         analyticsManager,
-      );
-
-      logger.info(
-        `${shortcut.user.id} requested a thread summarization on ${payload.message.ts} in channel ${payload.channel.id}`,
-      );
-
-      await threadSummarizer.summarize(
-        context.botId || '',
-        payload.team?.id || 'unknown',
-        payload.user.id,
-        {
-          type: 'thread',
-          threadTs: payload.message_ts,
-          channelId: payload.channel.id,
-          channelName: payload.channel.name,
-        },
+        threadSummarizer,
         client,
+        logger,
+        shortcut,
+        payload,
         respond,
+        context,
       );
     } catch (error) {
-      logger.error(`error in thread summarization: ${error.stack}`);
-
-      if ((error as Error).message.toLowerCase().includes('not_in_channel')) {
-        await addToChannelInstructions(
-          client,
-          shortcut.trigger_id,
-          {
-            channelId: payload.channel.id,
-            channelName: payload.channel.name,
-            currentUser: payload.user.id,
-            teamId: payload.user.team_id || 'unknown',
-            summarization: {
-              type: 'thread',
-              channelId: payload.channel.id,
-              threadTs: payload.message_ts,
-              channelName: payload.channel.name,
-            },
-          },
-          analyticsManager,
-          context.botUserId || '',
-        );
-        analyticsManager.threadSummaryFunnel({
-          funnelStep: 'not_in_channel',
-          slackTeamId: payload.team?.id || 'unknown',
-          slackUserId: payload.user.id,
-          channelId: payload.channel.id,
-          threadTs: payload.message_ts,
-        });
-        return;
-      }
-
-      if (
-        (error as Error).message.toLowerCase().includes('channel_not_found') ||
-        (error as Error).message.toLowerCase().includes('missing_scope')
-      ) {
-        await privateChannelInstructions(
-          client,
-          payload.trigger_id,
-          {
-            channelId: payload.channel.id,
-            channelName: payload.channel.name,
-            currentUser: payload.user.id,
-            teamId: payload.user.team_id || 'unknown',
-          },
-          analyticsManager,
-          context.botUserId || '',
-        );
-        analyticsManager.threadSummaryFunnel({
-          funnelStep: 'private_channel',
-          slackTeamId: payload.team?.id || 'unknown',
-          slackUserId: payload.user.id,
-          channelId: payload.channel.id,
-          threadTs: payload.message_ts,
-        });
-        return;
-      }
+      logger.error(`error in thread summarization handler: ${error.stack}`);
     }
   };
