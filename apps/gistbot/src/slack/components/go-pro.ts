43b174e76bfd1984515a137d2e48e201ba82eb6a
amirsr <amirsr212@gmail.com>
2022-12-28 19:30:05 +0200

    fixed text (#314)

--- apps/gistbot/src/slack/components/go-pro.ts
+++ apps/gistbot/src/slack/components/go-pro.ts
@@ -2,7 +2,8 @@ import { KnownBlock } from '@slack/web-api';
 import { GoProButton } from './go-pro-button';
 import { PromoCodeText } from './promo-code-text';
 
-export const GoProText = `You have exceeded your daily chat sessions limit. To get unlimited chat sessions and more features, become a pro member now with our early bird plan. ${PromoCodeText()}`;
+export const GoProText = `You have exceeded your daily summaries limit. To get more summaries per day, become a pro member now with our early bird plan. ${PromoCodeText()}`;
+
 export const GoPro = (): KnownBlock[] => {
   return [
     {
